1b41b4f484328343fe2e157792b31aa9c4145cd1
Sendil Kumar <sendilkumarn@live.com>
2018-01-22 23:47:01 +0100

    moving things to es6 syntax

--- index.js
+++ index.js
@@ -1,78 +1,83 @@
-var jscodeshift = require('jscodeshift');
-
+const jscodeshift = require('jscodeshift');
 
 function createArrowFunction(value) {
-	return '() => ' +  '\'' + value + '\'';
+	return `() => '${value}'`;
 }
+
 function createRegularFunction(value) {
-	return 'function () {\n return ' + '\'' + value + '\'' + '\n}';
+	return `function () {\n return '${value}'\n}`;
 }
+
 function createDynamicPromise(arr) {
 	if(Array.isArray(arr)) {
-		return '() => new Promise((resolve) => resolve([' + arr.map( (n) => {
-			return '\'' + n + '\'';
-		}) + ']))';
+		return `() => new Promise((resolve) => resolve([${arr.map( (n) => {
+    	return '\'' + n + '\'';
+		})}]))`;
 	} else {
-		return '() => new Promise((resolve) => resolve(' + '\'' + arr + '\'' + '))';
+		return `() => new Promise((resolve) => resolve('${arr}'))`;
 	}
 }
+
 function createAssetFilterFunction(value) {
-	return 'function (assetFilename) {\n return assetFilename.endsWith(' +  '\'' + '.' + value + '\'' + ');\n}';
+	return `function (assetFilename) {\n return assetFilename.endsWith('.${value}');\n}`;
 }
+
 function createExternalFunction(regexp) {
-	return '\n function (context, request, callback) {\n if ('
-	+ '/' + regexp + '/.test(request)){' + '\n' + '   return callback(null, \'commonjs\' + request);\n}\n'
-	+ 'callback();\n}';
+	return `\n function (context, request, callback) {\n if (/${regexp}/.test(request)){\n   return callback(null, 'commonjs' + request);\n}\ncallback();\n}`;
 }
+
 function parseValue(regexp) {
 	return jscodeshift(regexp);
 }
 
 function createCommonsChunkPlugin(value) {
-	return 'new webpack.optimize.CommonsChunkPlugin({name:' + '\'' + value + '\'' + ',filename:' + '\'' + value + '\-[hash].min.js\'})';
+	return `new webpack.optimize.CommonsChunkPlugin({name:'${value}',filename:'${value}\-[hash].min.js'})`;
 }
+
 function createRequire(val) {
-	return 'const ' + val + ' = ' + 'require(' + '\'' + val + '\'' + ');';
+	return `const ${val} = require('${val}');`;
 }
+
 function List(name, message, choices) {
 	return ({
 		type: 'list',
-		name: name,
-		message: message,
-		choices: choices
+		name,
+		message,
+		choices
 	});
 }
+
 function RawList(name, message, choices) {
 	return ({
 		type: 'rawlist',
-		name: name,
-		message: message,
-		choices: choices
+		name,
+		message,
+		choices
 	});
 }
 
 function CheckList(name, message, choices) {
 	return ({
 		type: 'checkbox',
-		name: name,
-		message: message,
-		choices: choices
+		name,
+		message,
+		choices
 	});
 }
 
 function Input(name, message) {
 	return ({
 		type: 'input',
-		name: name,
-		message: message
+		name,
+		message
 	});
 }
 
 function InputValidate(name, message, cb) {
 	return ({
 		type: 'input',
-		name: name,
-		message: message,
+		name,
+		message,
 		validate: cb
 	});
 }
@@ -80,8 +85,8 @@ function InputValidate(name, message, cb) {
 function Confirm(name, message) {
 	return ({
 		type: 'confirm',
-		name: name,
-		message: message,
+		name,
+		message,
 	});
 }
 
